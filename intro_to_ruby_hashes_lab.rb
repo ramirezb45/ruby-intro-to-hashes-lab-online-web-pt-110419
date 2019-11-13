8ac575bb63d67ec05a79ee56e7a045dc2948d005
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-11-13 21:09:55 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,16 @@
 def new_hash
-
+new_hash = Hash.new 
+{}
 end
 
 def actor
-
+actor = {:name=> "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {railroads: 
+	  
+	{
 end
 
 def monopoly_with_second_tier
